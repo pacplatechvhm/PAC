00296084c12213ebd596b9a8386d9b08d8ebb0b0
platechDev <platechDev@gmail.com>
2025-01-28 21:50:30 -0500

    crear componente de enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {ConsultarAdminComponent} from './feature/admin/consultar/consultar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    children: [
+      {path: 'consultar', component: ConsultarAdminComponent }
+    ]
+  },
+];
---
+++ src/app/feature/admin/consultar/consultar.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-consultar',
+  templateUrl: './consultar.component.html',
+  styleUrls: ['./consultar.component.scss'],
+  standalone: true,
+})
+export class ConsultarAdminComponent {}
